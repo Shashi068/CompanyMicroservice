2beeb59687af968f2594a6ea66757054ddac5cbd
Shashi <kiranshashi068@gmail.com>
2026-04-28 15:31:03 +0530

    Initial commit

---
+++ src/test/java/com/shashi/companyms/CompanymsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shashi.companyms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanymsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
