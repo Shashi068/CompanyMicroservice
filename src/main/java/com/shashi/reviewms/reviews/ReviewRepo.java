0b6aeeea95b4b34df1cee21990795a0453bd85ae
Shashi Kiran <88266901+Shashi068@users.noreply.github.com>
2026-04-28 15:37:33 +0530

    Delete src/main/java/com/shashi/reviewms directory

--- src/main/java/com/shashi/reviewms/reviews/ReviewRepo.java
+++
@@ -1,9 +0,0 @@
-package com.shashi.reviewms.reviews;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface ReviewRepo extends JpaRepository<Review, Long> {
-    List<Review> findByCompany_Id(Long companyId);
-}
